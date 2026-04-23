b23842f1c0394c1b18cfee1a5fe35fd3984c2805
Kon-sys <likhtarilya14112005@gmail.com>
2026-04-23 19:31:48 +0300

    init project

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+const React = require("react");
+
+function App() {
+    return <div>Пустой проект</div>;
+}
+
+module.exports = App;
+module.exports.default = App;
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+        clean: true
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        sourceType: "unambiguous",
+                        presets: [
+                            ["@babel/preset-env"],
+                            ["@babel/preset-react", { runtime: "classic" }]
+                        ],
+                        plugins: ["@babel/plugin-transform-modules-commonjs"]
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        })
+    ],
+    devServer: {
+        static: path.resolve(__dirname, "dist"),
+        port: 3000,
+        open: true,
+        hot: true
+    }
+};
